64a6ba2e445991a27edee4482a57c79cd064c066
ilcfloran <ilcfloran>
2017-12-29 01:48:26 +0100

    Add project files.

---
+++ TT-Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TT_Task2.Models;
+
+namespace TT_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Program Started");
+            System.Console.WriteLine("**********************");
+
+            RootObject result = new RootObject();
+            try
+            {
+                Service service = new TT_Task2.Service();
+                // call another helper to map to the Person
+                service.callingAsync().Wait();
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("failed attempt");
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
